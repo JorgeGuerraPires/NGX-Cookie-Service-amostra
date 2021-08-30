b1785633759717d5cc7b6493c8aef9e8060c7537
Jorge Pires <jorgeguerrapires@yahoo.com.br>
2021-08-30 07:31:00 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  cookieValue;
+
+  constructor(private cookieService: CookieService
+  ) {
+    this.cookieService.set('Test', 'Hello World, this is Jorge Guerra Pires talking to you');
+    this.cookieValue = this.cookieService.get('Test');
+  }
+
+
 }
